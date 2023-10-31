fdf90f56eedd9c664d3a0d0cb08c867c7a6214e5
I-zy-github <i-zy@monolith-factory.com>
2023-10-31 10:38:47 +0900

    AuthFile

--- API2Json.py
+++ API2Json.py
@@ -1,11 +1,13 @@
 #!python3.10
 # import json
 import streamlit as st
+from google.oauth2 import service_account
 import requests
 import pandas as pd
 import gspread
 from gspread_dataframe import get_as_dataframe, set_with_dataframe
 import os
+
 
 st.title("外部APIをスプレッドシートに反映する")
 st.caption("下部にURLをスプシKeyを入力")
@@ -24,14 +26,14 @@ st.text(df)
 dir_path = os.path.dirname(__file__) # 作業フォルダの取得
 gc = gspread.oauth(
                    credentials_filename=os.path.join(dir_path, "client_secret_1011947998521-51un23r1d4b42ihjk4kegs999v846f4b.apps.googleusercontent.com.json"), # 認証用のJSONファイル
-                   authorized_user_filename=os.path.join(dir_path, "C:/Users/I-zy/PycharmProjects/Gsheets/authorized_user.json"), # 証明書の出力ファイル
+                   authorized_user_filename=os.path.join(dir_path, "authorized_user.json"), # 証明書の出力ファイル
                    )
 
 # スプレッドシートに書き込み
 wb = gc.open_by_key(GSkey) # test02のファイルを開く(キーから)
-#ws = wb.get_worksheet(0) # 最初のシートを開く(idは0始まりの整数)
+ws = wb.get_worksheet(0) # 最初のシートを開く(idは0始まりの整数)
 
 data = df
-#set_with_dataframe(ws,data.reset_index())
+set_with_dataframe(ws,data.reset_index())
 # 複数行一括書き込み
 # wb.values_append("シート1", {"valueInputOption": "USER_ENTERED"}, {"values": data})
